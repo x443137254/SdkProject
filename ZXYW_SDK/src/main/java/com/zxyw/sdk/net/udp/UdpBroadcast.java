093773a9213dd772633d241272ea3b14f1e70092
zxyw_xmj <zh123456>
2023-01-09 11:35:56 +0800

    2.4 更新UDP工具类，udp发送改为定时5s循环发送，单独线程循环监听，本地发送端口随机，接收端口与发送端口一致，增加停止的方法

--- ZXYW_SDK/src/main/java/com/zxyw/sdk/net/udp/UdpBroadcast.java
+++ ZXYW_SDK/src/main/java/com/zxyw/sdk/net/udp/UdpBroadcast.java
@@ -1,27 +1,69 @@
 package com.zxyw.sdk.net.udp;
 
+import android.os.Handler;
+import android.os.HandlerThread;
+
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.util.Arrays;
 
 public class UdpBroadcast {
+    private final Thread receiveThread;
+    private DatagramSocket socket;
+    private boolean running;
+    private final Handler handler;
 
-    public void send(int port, byte[] data, UdpListener listener) {
-        new Thread(() -> {
-            try {
-                DatagramPacket datagramPacket = new DatagramPacket(data, 0, data.length, InetAddress.getByName("255.255.255.255"), port);
-                DatagramSocket socket = new DatagramSocket();
-                socket.send(datagramPacket);
-                if (listener != null) {
-                    byte[] buff = new byte[1024];
-                    final DatagramPacket recPacket = new DatagramPacket(buff, buff.length);
-                    socket.receive(recPacket);
+    public UdpBroadcast(UdpListener listener) {
+        try {
+            socket = new DatagramSocket();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        running = true;
+        receiveThread = new Thread(() -> {
+            if (listener != null) {
+                final byte[] buff = new byte[1024];
+                DatagramPacket recPacket;
+                while (running) {
+                    recPacket = new DatagramPacket(buff, buff.length);
+                    try {
+                        socket.receive(recPacket);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        continue;
+                    }
                     listener.onReceive(Arrays.copyOfRange(recPacket.getData(), 0, recPacket.getLength()));
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-        }).start();
+        });
+        receiveThread.start();
+        final HandlerThread thread = new HandlerThread("");
+        thread.start();
+        handler = new Handler(thread.getLooper());
+    }
+
+    public void send(int port, byte[] data) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (socket != null) {
+                    try {
+                        socket.send(new DatagramPacket(data, 0, data.length, InetAddress.getByName("255.255.255.255"), port));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                handler.postDelayed(this, 5000);
+            }
+        });
+    }
+
+    public void stopBroadcast() {
+        handler.removeCallbacksAndMessages(null);
+        running = false;
+        receiveThread.interrupt();
     }
 }
